691673e5335c9f1491cfe6c6d5b06d7edbccd5b1
박도희 <dohee.pa@gmail.com>
2021-02-18 22:22:59 +0900

    11.4 React.memo를 사용하어 컴포넌트 성능 최적화

--- src/App.js
+++ src/App.js
@@ -3,24 +3,20 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false
+    });
+  }
+  return array;
+}
+
 const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: 'learn react',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: 'styling component',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: 'build a todo app',
-      checked: false,
-    },
-  ]);
+  const [todos, setTodos] = useState(createBulkTodos);
 
   const nextId = useRef(4);
 
