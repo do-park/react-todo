7a59e58731376e924aefcdc8bc705ba6d5e018d8
박도희 <dohee.pa@gmail.com>
2021-02-19 21:58:11 +0900

    11.8 react-virtualized를 사용한 렌더링 최적화

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,10 +7,10 @@ import {
 import cn from 'classnames';
 import './TodoListItem.scss';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
   return (
-    <div className="TodoListItem">
+    <div className="TodoListItem-virtualized" style={style}>
       <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
         {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
         <div className="text">{text}</div>
@@ -22,4 +22,4 @@ const TodoListItem = ({ todo, onRemove, onToggle }) => {
   );
 };
 
-export default React.memo(TodoListItem);
+export default React.memo(TodoListItem, (prevProps, nextProps) => prevProps.todo === nextProps.todo,);
